ad3d890367208586a0acc8e7b7a5104b218b9fe7
ssimmons122 <121777930+ssimmons122@users.noreply.github.com>
2023-04-02 17:32:28 -0400

    shapes done?

--- main/lib/shapes.js
+++ main/lib/shapes.js
@@ -15,4 +15,11 @@ class Circle extends Shape {
 }
 class Triangle extends Shape {
     render() {
-        return `<triangle
+        return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
+    }
+}
+class Square extends Shape {
+    render() {
+        return `<rect x="90" y="40" width="120" height="120" fill="${this.color}" />`;
+    }
+}
